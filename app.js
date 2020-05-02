4fa60c74e4c3f31fa8a96e6d91ee53f9c3129739
Don <donli90@hotmail.com>
2020-05-02 13:39:00 -0700

    .

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const fbService = {
   "type": process.env.type || firebaseService.type,
   "project_id": process.env.project_id || firebaseService.project_id,
   "private_key_id": process.env.private_key_id || firebaseService.private_key_id,
-  "private_key": process.env.private_key || firebaseService.private_key,
+  "private_key": process.env.private_key.replace(/\\n/g, '\n') || firebaseService.private_key,
   "client_email": process.env.client_email || firebaseService.client_email,
   "client_id": process.env.client_id || firebaseService.client_id,
   "auth_uri": process.env.auth_uri || firebaseService.auth_uri,
